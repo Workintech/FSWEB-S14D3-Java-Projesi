4a0feabaf41932d323820868bc405b85ca9bb1ca
samilkafa <samilkafa@gmail.com>
2023-08-16 01:05:51 +0300

    Hafta-18-Proje-3

---
+++ src/com/workintech/polymorphism/rnd/ElectricCar.java
@@ -0,0 +1,23 @@
+package com.workintech.polymorphism.rnd;
+
+import com.workintech.polymorphism.skeleton.CarSkeleton;
+
+public class ElectricCar extends CarSkeleton {
+
+    private double avgKmPerCharge;
+    private int batterySize;
+
+    public ElectricCar(String name, String description, double avgKmPerCharge, int batterySize) {
+        super(name, description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    public double getAvgKmPerCharge() {
+        return avgKmPerCharge;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+}
---
+++ src/com/workintech/polymorphism/skeleton/CarSkeleton.java
@@ -0,0 +1,49 @@
+package com.workintech.polymorphism.skeleton;
+
+import com.workintech.polymorphism.rnd.ElectricCar;
+import com.workintech.polymorphism.rnd.GasPoweredCar;
+import com.workintech.polymorphism.rnd.HybridCar;
+
+public class CarSkeleton {
+
+    private String name;
+    private String description;
+
+    public CarSkeleton(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String startEngine() {
+        System.out.println("Class name: " + getClass().getSimpleName());
+        return getName() + " is starting engine";
+    }
+
+    public String drive() {
+        runEngine(this);
+        return getName() + " is driving";
+    }
+
+    protected void runEngine(CarSkeleton carSkeleton) {
+        if (carSkeleton instanceof GasPoweredCar) {
+            ((GasPoweredCar)carSkeleton).getAvgKmPerLitre();
+            System.out.println("The car engine is starting with gas");
+        } else if (carSkeleton instanceof HybridCar) {
+            ((HybridCar)carSkeleton).getAvgKmPerLitre();
+            System.out.println("The car engine is starting with both gas and electricity");
+        } else if (carSkeleton instanceof ElectricCar) {
+            ((ElectricCar)carSkeleton).getAvgKmPerCharge();
+            System.out.println("The car engine is starting with electricity");
+        } else {
+            System.out.println("Invalid car type");
+        }
+    }
+}
